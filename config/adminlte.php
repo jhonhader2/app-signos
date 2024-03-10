8dcc22d7e100827271eff8724101e19715c83706
Jhon Hader Rodriguez Perdomo <jhonhader2@hotmail.com>
2024-03-10 13:03:54 -0500

    Modificacion de Ruta Inicial

--- config/adminlte.php
+++ config/adminlte.php
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
+    'logo' => env('APP_NAME'),
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
